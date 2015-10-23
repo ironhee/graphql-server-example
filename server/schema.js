5e5e3b88b59c9f60b3e59bbaedf998e31adc98c6
ironhee <leechulhee95@gmail.com>
2015-10-23 15:55:49 +0900

    Implement draft prototype

--- server/schema.js
+++ server/schema.js
@@ -1,23 +1,85 @@
 import {
   GraphQLObjectType,
   GraphQLSchema,
-  GraphQLInt,
+  GraphQLString,
 } from 'graphql';
 
-const count = 0;
+import {
+  nodeDefinitions,
+  connectionDefinitions,
+  connectionArgs,
+  connectionFromArray,
+  globalIdField,
+  fromGlobalId,
+} from 'graphql-relay';
+
+import uuid from 'node-uuid';
+import _ from 'lodash';
+
+const drafts = [
+  {
+    id: uuid.v4(),
+    content: 'abcd',
+  },
+  {
+    id: uuid.v4(),
+    content: 'foo',
+  },
+];
+
+function getDraft(id) {
+  return _.find(drafts, { id });
+}
+
+function getDrafts() {
+  return drafts;
+}
+
+const {nodeInterface, nodeField} = nodeDefinitions(
+  (globalId) => {
+    const {type, id} = fromGlobalId(globalId);
+    switch (type) {
+    case 'Draft':
+      return getDraft(id);
+    default:
+      return null;
+    }
+  },
+  (/* obj */) => {
+    return draftType;
+  }
+);
+
+const draftType = new GraphQLObjectType({
+  name: 'Draft',
+  fields: {
+    id: globalIdField('Draft'),
+    content: {
+      type: GraphQLString,
+    },
+  },
+  interfaces: [nodeInterface],
+});
+
+const {connectionType: draftConnection} =
+  connectionDefinitions({name: 'Draft', nodeType: draftType});
+
+const queryType = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    node: nodeField,
+    drafts: {
+      type: draftConnection,
+      args: connectionArgs,
+      resolve: (root, args) => connectionFromArray(
+        getDrafts(),
+        args
+      ),
+    },
+  },
+});
 
 export default new GraphQLSchema({
-  query: new GraphQLObjectType({
-    name: 'RootQueryType',
-    fields: {
-      count: {
-        type: GraphQLInt,
-        resolve(parentValue, _, { rootValue: { uid } }) {
-          console.log(uid);
-          return count;
-        },
-      },
-    },
-  }),
+  query: queryType,
 });
 
