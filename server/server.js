eca49637cb0e78d24cd8b4cddb13bf6f7344a31a
ironhee <iron@ediket.com>
2015-11-18 17:30:26 +0900

    group related things.

--- server/server.js
+++ server/server.js
@@ -7,25 +7,33 @@ import Schema from './schema';
 import { router as authRouter } from './rest/auth';
 
 
-// Expose a GraphQL endpoint
 export function run() {
   const server = express();
-  server.use(jwt(JWT_SECRET));
-  server.use(bodyParser.urlencoded({ extended: false }));
-  server.use(bodyParser.json());
-  server.use('/', [
-    authRouter,
-    graphqlHTTP(
-      request => ({
-        schema: Schema,
-        pretty: true,
-        rootValue: {
-          uid: request.token ? request.token.userid : null,
-        },
-        graphiql: true,
-      })
-    ),
+
+  // Middlewares
+  server.use([
+    jwt(JWT_SECRET),
+    bodyParser.urlencoded({ extended: false }),
+    bodyParser.json(),
   ]);
+
+  // REST Routers
+  server.use([
+    authRouter,
+  ]);
+
+  // GraphQL
+  server.use('/', graphqlHTTP(
+    request => ({
+      schema: Schema,
+      pretty: true,
+      rootValue: {
+        uid: request.token ? request.token.userid : null,
+      },
+      graphiql: true,
+    })
+  ));
+
   server.listen(GRAPHQL_PORT, () => {
     console.log(`Server is now running on http://localhost:${GRAPHQL_PORT}`);
   });
